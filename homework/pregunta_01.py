0e72ee09e91dfdc9a74767265b8f007e22b0771b
Juan Alejandro Espinosa <65057841+alejoec03@users.noreply.github.com>
2025-12-03 21:46:09 -0500

    sln

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,6 +3,9 @@ Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
 
 
+
+
+
 def pregunta_01():
     """
     Realice la limpieza del archivo "files/input/solicitudes_de_credito.csv".
@@ -13,3 +16,33 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+
+    import pandas as pd
+    import os
+
+    df = pd.read_csv("files/input/solicitudes_de_credito.csv", index_col=0, sep=";")
+    df.dropna(axis=0, how="any", inplace=True)
+
+    #Sexo
+    df["sexo"] = df["sexo"].str.lower()
+    
+    #Fecha
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"],dayfirst=True,format="mixed", errors="coerce")
+
+    #Monto credito
+    df["monto_del_credito"] = (df["monto_del_credito"].str.replace("$", "", regex=False).str.replace(",", "", regex=False).astype(float))
+
+    #unificación de categorias
+    columnas_unificables = ["tipo_de_emprendimiento","idea_negocio","barrio","línea_credito",]
+
+    for col in columnas_unificables:
+        df[col] = df[col].str.lower().str.replace(r"[ .-]", "_", regex=True).str.strip()
+
+    df.drop_duplicates(inplace=True)
+
+    if not os.path.exists("files/output/"):
+        os.makedirs("files/output/")
+
+    df.to_csv(
+        "files/output/solicitudes_de_credito.csv", sep=";", header=True, index=False
+    )
